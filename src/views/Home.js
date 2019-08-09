2361f0ff93b7025b400a4e41e81688f8ec240a74
Dan Zinn <danzinn@Dans-MacBook-Pro.local>
2019-08-08 18:58:51 -0600

    updated dates for 2019

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,7 +7,6 @@ const Home = () => (
 		<p>D'Evelyn Youth Basketball Association is a club that provides an opportunity for Dennison and D’Evelyn girls and boys to play league basketball. We have teams from 3rd through 8th grade. Our 3rd through 8th grade teams play in the Jeffco Basketball Conference. Some of our 4th through 8th grade teams will also play in the Gold Crown league. </p>
 		<p>Our team placement is free to attend and our annual membership dues are $50. These funds are used to cover the costs of running the club. You must be a member of the club to play on our teams.</p>
 		<p>2019 Team Placement is on Sunday, September 8th. Click on the Registration button to get started. Click on the Team Placement button for this year's schedule.</p>
-		<p>* We encourage kindergartners and 1st graders to attend the annual team placement to have some fun, get a t-shirt and get some experience with the process. Most of these kids will not be placed on a DYBA team. Depending on size and skill level, 1st graders may be invited to play on a 2nd grade team. For the rest, the YMCA league is a good option.</p>
 		<div className="text-center">
 			<a className="link-button btn inline" href="/register">Registration</a>
 			<a className="link-button btn inline" href="/team-placement">Team Placement</a>
