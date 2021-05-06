5963bc7802c09c114d955fc7de67f983774aae28
develynyouthbasketball <77304927+develynyouthbasketball@users.noreply.github.com>
2021-05-06 11:52:56 -0600

    Update SummerCamp.js

--- src/views/SummerCamp.js
+++ src/views/SummerCamp.js
@@ -4,10 +4,22 @@ import brochure from '../images//2019-DYBA-Summer-Camps.png';
 
 const SummerCamp = () => (
   <MainContainer title="PROGRAMS">
-    <h2>Summer Camps 2021</h2>
-    <p>Grades 2-5 - Coed - June 1,2,3 from 9-11am
-Grades 6-8 - Boys - June 1,2,3 from 11:30-1:30pm
-Grades 6-8 - Girls - June 7,8,9 from 12-2pm</p>
+    <h2>Summer Camps 2021 - $90</h2>
+    <ol>
+      Grades 2-5 - Coed - June 1,2,3 from 9-11am
+      Grades 6-8 - Boys - June 1,2,3 from 11:30-1:30pm
+      Grades 6-8 - Girls - June 7,8,9 from 12-2pm
+    </ol>
+  <p>Advanced registration is required.</p>
+    <p className="center-margin-top">Click the Register button to begin the process.</p>
+    <a
+      className="link-button btn"
+      href="https://registration.bluesombrero.com/12806/available-programs"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Register
+    </a>
   </MainContainer>
 );
 
